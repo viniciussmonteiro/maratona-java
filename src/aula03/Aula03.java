97d6b0baa274eb7ee7162d5420d10274d283da9c
viniciussmonteiro <viniciusmonteiro5602@gmail.com>
2025-07-24 23:31:53 -0300

    Adiciona Aula 03: Estruturas condicionais, If, Else, Else If, Switch

--- src/aula03/Aula03.java
+++ src/aula03/Aula03.java
@@ -1,4 +1,25 @@
 package aula03;
 
 public class Aula03 {
+
+// Estruturas condicionais: If, Else, Else If,Switch
+
+    public static void main(String[] args) {
+
+        int idade = 20;
+        boolean maiorDeIdade = idade >= 18;
+        // !
+
+        if (maiorDeIdade) {
+            System.out.println("Autorizado a comprar bebida alcólica.");
+        }
+
+        if (!maiorDeIdade) {
+            System.out.println("Não autorizado a comprar bebida alcólica.");
+        }
+
+
+        
+
+    }
 }
