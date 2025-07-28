474c2278005af70ad9614111bccf662e6af505e7
viniciussmonteiro <viniciusmonteiro5602@gmail.com>
2025-07-27 23:55:08 -0300

    Adiciona Aula 05: Classes e Objetos

---
+++ src/aula05/Aula05.java
@@ -0,0 +1,21 @@
+package aula05;
+
+public class Aula05 {
+
+    //Aula 05 - Classes e Objetos
+
+    public static void main(String[] args) {
+        // Exemplo de uso da classe Pessoa
+        Pessoa pessoa = new Pessoa("João", 30);
+        System.out.println(pessoa.getNome() + " tem " + pessoa.getIdade() + " anos.");
+
+        // Exemplo de uso da classe Carro
+        Carro carro = new Carro("Fusca", "Volkswagen");
+        System.out.println("O carro é um " + carro.getModelo() + " da marca " + carro.getMarca() + ".");
+
+
+
+    }
+
+
+}
